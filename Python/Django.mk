c3b91379ca0c51783415d5110160447923db833e
herohe910623 <61967411+herohe910623@users.noreply.github.com>
2025-09-10 14:57:43 +0900

    Update Django.mk

--- Python/Django.mk
+++ Python/Django.mk
@@ -1,6 +1,6 @@
 **Django** 웹 프레임워크 (파이썬) 이란
 
-다음 페이지를 인용하였습니다 / [1] : https://developer.mozilla.org/ko/docs/Learn_web_development/Extensions/Server-side/Django
+다음 페이지를 인용하였습니다 \ [1] : https://developer.mozilla.org/ko/docs/Learn_web_development/Extensions/Server-side/Django
 
 Django 는 파이썬으로 구성된, 인기 많고 완벽한 기능을 갖춘 서버-사이드 웹 프레임워크 입니다. 이 모듈은 
 Django가 웹 서버 프레임워크 중 가장 유명한 이유, 개발환경을 설정하는방법, 그리고 이를 통해 자신만의 웹 애플리케이션을 만드는 방법을 알려줍니다.
